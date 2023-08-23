355c0cc1f026a3d45dbf65f3605832a58efb8740
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-08-23 15:17:57 +0200

    feat: conditional render on text

--- src/App.jsx
+++ src/App.jsx
@@ -119,15 +119,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
   const numItems = items.length;
-  const packedItems = items.filter(item => item.packed).length;
-  const percentItems = Math.floor((packedItems / numItems) * 100);
+  const numPacked = items.filter(item => item.packed).length;
+  const percentItems = Math.round((numPacked / numItems) * 100);
 
   return (
     <footer className="stats">
       <em>
-        You have {numItems} items on your list, and you already packed{' '}
-        {packedItems} ({!percentItems ? '0' : percentItems}%)
+        {percentItems === 100
+          ? 'You got everything! Ready to go ✈'
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${!percentItems ? '0' : percentItems}%)`}
       </em>
     </footer>
   );
